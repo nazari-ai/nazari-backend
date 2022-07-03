900d43eb007b74567ea8d170a1aeb6441f61a8fa
Precillieo <kolawoleprecious99@gmail.com>
2022-07-03 20:55:34 +0100

    migration: new_db

--- API/github_code/api/github.py
+++ API/github_code/api/github.py
@@ -23,7 +23,7 @@ class GithubAnalyticsPerTime:
     pull_requests: int
     issues: int
     watches:int
-    lp_day_of_week: Union[int, None]
+    lp_day_of_week: Union[str, None]
     lp_day: Union[int, None]
     last_push_date: Union[datetime.datetime, None]
 
