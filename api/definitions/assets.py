a1de280eef569b36db0f752224af4c92d20889b1
stevekola <kolawolesteven99@gmail.com>
2022-11-04 12:15:50 +0100

    implement pagination schema

--- api/definitions/assets.py
+++ api/definitions/assets.py
@@ -27,6 +27,33 @@ class AsaData:
     reddit: Optional[str]
     github: Optional[str]
     available: bool
+
+
+@strawberry.type
+class AsaDataPagination:
+    asset_id: str
+    name: str
+    logo: Optional[str]
+    unitname_1: Optional[str]
+    unitname_2: Optional[str]
+    reputation__pera: str
+    reputation__algoexplorer: str
+    score__algoexplorer: int
+    description: Optional[str]
+    URL: Optional[str]
+    usd_value: Optional[str]
+    fraction_decimals: Optional[int]
+    total_supply: Optional[str]
+    circ_supply: Optional[str]
+    category: Optional[str]
+    creator: Optional[str]
+    twitter: Optional[str]
+    telegram: Optional[str]
+    discord: Optional[str]
+    medium: Optional[str]
+    reddit: Optional[str]
+    github: Optional[str]
+    available: bool
     asset_total: int
 
 
